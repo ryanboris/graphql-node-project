7dbaefb342ace3b67cca91f7b90bae20be30a439
ryanboris <ryan.boris@gmail.com>
2018-12-21 14:48:53 -0500

    Adds location and bio schema and resolvers

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import { GraphQLServer } from 'graphql-yoga'
 const typeDefs = `
     type Query {
         hello: String!
+        name: String!
+        location: String!
+        bio: String!
     }
 `
 
@@ -15,10 +18,26 @@ const resolvers = {
         hello() {
             return 'Hello!'
         },
+
+        name() {
+            return 'Ryan Boris!'
+        },
+
+        location() {
+            return 'Philadelphia'
+        },
+
+        bio() {
+            return 'I love GraphQL!'
+        },
     },
 }
 
 const server = new GraphQLServer({
     typeDefs,
     resolvers,
+})
+
+server.start(() => {
+    console.log('Server is running!')
 })
