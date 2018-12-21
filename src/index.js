2b71ca0d6d66d9a75f0976d2ff8126e0c1d61aa5
ryanboris <ryan.boris@gmail.com>
2018-12-21 00:14:35 -0500

    Adds tutorial on ES6 imports/exports

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log('Hello GraphQL')
+import currentLocation, { message, name } from "./myModule";
+
+console.log(message);
+console.log(name);
+console.log(currentLocation);
+
+/**
+ * default: no {} can be any name
+ */
+
+ /**
+  * named: {named1, named2}
+  */
---
+++ src/myModule.js
@@ -0,0 +1,16 @@
+const message = "Some message from myModule.js";
+
+const name = "Ryan";
+
+const location = 'Philadelphia'
+
+export { message, name, location as default };
+
+/**
+ * named exports - can have as many as needed
+ */
+
+
+ /**
+  * default export - no name, only one
+  */
